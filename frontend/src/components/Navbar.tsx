5a37740a1c81b4d1ac3019708d25a9381866d4c5
bubbletea03 <bubbletea03@naver.com>
2023-03-11 22:21:42 +0900

    feat: Navbar에 아이콘 추가

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,24 +3,41 @@ import styled from "styled-components";
 export default function Navbar() {
   return (
     <Wrapper>
-      <div>MainPage</div>
-      <div>Account</div>
-      <div>My Profile</div>
-      <div>Credits</div>
+      <NavButton>
+        <Icon src="/icons/home.png" alt="홈 아이콘" />
+        <div>MainPage</div>
+      </NavButton>
+      <NavButton>
+        <Icon src="/icons/person.png" alt="사람 아이콘" />
+        <div>My Profile</div>
+      </NavButton>
+      <NavButton>
+        <Icon src="/icons/board.png" alt="게시판 아이콘" />
+        <div>Credits</div>
+      </NavButton>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  position: absolute;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 80px;
   height: 100%;
   background-color: #3c3c3c;
-  > * {
-    margin-top: 50px;
-    color: lightgray;
-    font-size: 14px;
-  }
+`;
+
+const NavButton = styled.button`
+  width: 100%;
+  margin-top: 50px;
+  color: lightgray;
+  text-align: center;
+  font-size: 14px;
+`;
+
+const Icon = styled.img`
+  width: 40px;
+  height: 40px;
 `;
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -4,44 +4,18 @@ import styled from "styled-components";
 export default function MainPage() {
   return (
     <>
-      <Background>
-        <GrayBox />
-        <TitleBox>D.velopers</TitleBox>
-      </Background>
-      <Wrapper>
-        <Navbar />
-        <section>
-          <div>ㅇㅇ</div>
-        </section>
-      </Wrapper>
+      <Navbar />
+      <Section>
+        <div>ㅇㅇ</div>
+      </Section>
     </>
   );
 }
 
-const Wrapper = styled.div`
-  position: absolute;
-  display: flex;
+const Section = styled.section`
+  display: block;
+  position: relative;
   width: 100%;
   height: 100%;
-`;
-
-const Background = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-`;
-
-const GrayBox = styled.div`
-  display: block;
-  position: static;
-  width: 100%;
-  height: 30%;
-  background-color: lightgray;
-`;
-
-const TitleBox = styled.div`
-  display: block;
-  position: static;
-  width: 100%;
-  height: 50px;
+  margin-left: 80px;
 `;
