f83a07daea7e5a3af919f7555d2349e141c6666e
bubbletea03 <bubbletea03@naver.com>
2023-03-17 18:59:02 +0900

    feat: Nav 하이라이트 이쁘게 함

--- frontend/src/components/navbar/Nav.tsx
+++ frontend/src/components/navbar/Nav.tsx
@@ -38,9 +38,11 @@ export default function Nav({ children, path }: PropsWithChildren<IProps>) {
 
 const Wrapper = styled.div<{ highlight: boolean }>`
   width: 100%;
-  margin-top: 50px;
+  margin-top: 30px;
+  padding: 20px 0;
   color: lightgray;
   text-align: center;
   font-size: 14px;
-  ${({ highlight }) => highlight && "background-color: white"};
+  ${({ highlight }) => highlight && "background-color: gray"};
+  transition: 1s;
 `;
