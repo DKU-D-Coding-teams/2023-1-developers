c12bac3c332c5779a957dcb2b6e189ef6f0404ef
dongsuu <suudh99@naver.com>
2023-03-28 02:36:29 +0900

    add: exception handling with RestControllerAdvice #27

--- backend/src/main/java/backend/dankook/exception/DankookException.java
+++ backend/src/main/java/backend/dankook/exception/DankookException.java
@@ -1,7 +1,8 @@
 package backend.dankook.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
+@Getter
 public class DankookException extends RuntimeException{
     private HttpStatus httpStatus;
     private String message;
---
+++ backend/src/main/java/backend/dankook/exception/ErrorResult.java
@@ -0,0 +1,12 @@
+package backend.dankook.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResult {
+    private HttpStatus status;
+    private String message;
+}
