8d38562bc78c90c390c108b5647b85d9443fb194
bubbletea03 <bubbletea03@naver.com>
2023-04-03 21:13:45 +0900

    feat: api 함수 작성

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { RegisterParams } from "./params";
 
 // axios.defaults.baseURL = "http://3.39.41.33:8080";
 
-export const postEmailCheck = (email: string) =>
-  axios.post("/members/mailCheck", {
-    email,
-  });
+export const postEmailCheck = (email: string) => axios.post("/members/mailCheck", JSON.stringify({ email }));
+
+export const postMemberRegister = (data: RegisterParams) => axios.post("/members/join", JSON.stringify(data));
+
+export const postMemberLogin = (email: string, password: string) =>
+  axios.post("/members/login", JSON.stringify({ email, password }));
