f9f31092677e94b8634ea012a1e869f884c8709d
bubbletea03 <bubbletea03@naver.com>
2023-04-09 22:20:04 +0900

    feat: 마크다운 Textarea 개선

--- frontend/src/components/profile/MarkdownTextarea.tsx
+++ frontend/src/components/profile/MarkdownTextarea.tsx
@@ -10,33 +10,50 @@ interface Props {
 }
 
 export default function MarkdownTextarea({ value, set }: Props) {
-  const [isEditMode, setEditMode] = useState(true);
-
-  let editTimeout: NodeJS.Timeout;
+  const [isPreviewMode, setPreviewMode] = useState(false);
 
   const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
     set(e.target.value);
   };
 
-  const activeEditMode = () => {
-    setEditMode(true);
-    clearTimeout(editTimeout);
-    editTimeout = setTimeout(() => setEditMode(false), 1000);
-  };
-
   return (
     <>
       <Wrapper>
-        <Textarea onChange={handleInput} onKeyDown={activeEditMode} onMouseDown={activeEditMode} />
-        {!isEditMode && value && (
+        <Title>자유 자기소개 (마크다운 지원)</Title>
+        <Textarea value={value} onChange={handleInput} />
+        {isPreviewMode && (
           <PreviewBox>
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{value}</ReactMarkdown>
           </PreviewBox>
         )}
+        <PreviewButton isPreviewMode={isPreviewMode} onClick={() => setPreviewMode(!isPreviewMode)}>
+          미리보기
+        </PreviewButton>
       </Wrapper>
     </>
   );
 }
+
+const PreviewButton = styled.button<{ isPreviewMode: boolean }>`
+  position: absolute;
+  top: -40px;
+  right: 35px;
+  width: 120px;
+  height: 45px;
+  line-height: 20px;
+  padding-bottom: 40px;
+  background-color: ${({ isPreviewMode }) => (isPreviewMode ? "gray" : "white")};
+  transition: background-color 0.5s;
+  text-align: center;
+  border-radius: 30px;
+  border: 1px solid gray;
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: -25px;
+  font-size: 1.1rem;
+`;
 
 const Wrapper = styled.div`
   position: relative;
@@ -46,6 +63,8 @@ const Wrapper = styled.div`
 `;
 
 const Textarea = styled(TextareaAutosize)`
+  position: relative;
+  z-index: 1;
   background-color: lightgray;
   border: 1px solid gray;
   border-radius: 20px;
@@ -58,19 +77,14 @@ const Textarea = styled(TextareaAutosize)`
 
 const PreviewBox = styled.div`
   position: absolute;
+  z-index: 1;
   top: 0;
+  bottom: 0;
   right: 0;
-
-  padding: 20px;
-  font-size: 1rem;
+  left: 0;
+  margin: 20px;
   background-color: lightgray;
-  border-radius: 40px;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  white-space: normal;
-
-  pointer-events: none;
+  font-size: 1.1rem;
 
   p {
     margin: 0;
